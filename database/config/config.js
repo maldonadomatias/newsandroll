72541b37051cf328f391dcffa1a608766dc852e0
maldonadomatias <molporron@gmail.com>
2021-12-01 09:41:33 -0300

    config

--- database/config/config.js
+++ database/config/config.js
@@ -7,17 +7,17 @@ module.exports = {
     "dialect": "mysql"
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
+    "username": "b4bff70bb68acb",
+    "password": "592118fd",
+    "database": "heroku_b0bf62bde946e9a",
+    "host": "us-cdbr-east-04.cleardb.com",
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": "b4bff70bb68acb",
+    "password": "592118fd",
+    "database": "heroku_b0bf62bde946e9a",
+    "host": "us-cdbr-east-04.cleardb.com",
     "dialect": "mysql"
   }
 }
