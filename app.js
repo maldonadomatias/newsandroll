742de95dcdbae8defeff65b38070f6a8eac036e6
maldonadomatias <molporron@gmail.com>
2021-12-01 12:10:57 -0300

    express

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const session = require('cookie-session');
+const session = require('express-session');
 const cookies = require('cookie-parser');
 const { Pool } = require('pg');
 
